c9391781b0d628f00316cd73bfc036c09727edfc
Anglo Essam Saber <eng.angloesam2010@gmail.com>
2020-04-05 20:12:24 +0300

    Memento design pattern

---
+++ Memnto.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+
+class Memento {
+    private var actionArray = [String]()
+    
+    public init(actionArray : [String] ){
+        self.actionArray = actionArray
+    }
+    
+    public func getActionArray()  -> [String]{
+        return self.actionArray
+    }
+    
+    public func setActionArray(actionArray : [String]) {
+        self.actionArray = actionArray
+    }
+    
+    
+}
+
+class Originator {
+    private var actionArray = [String]()
+    
+    public func setActionArray(actionArray : [String]) {
+        self.actionArray = actionArray
+    }
+    
+    public func save() -> Memento {
+        let m = Memento(actionArray: self.actionArray)
+        return m
+    }
+    public func restore(m : Memento) {
+        self.actionArray = m.getActionArray()
+    }
+    
+}
+
+class Caretaker {
+    private var array = [Memento]()
+    public func addMemnto(m : Memento) {
+        self.array.append(m)
+    }
+    public func getMemento() -> Memento {
+        self.array[1]
+    }
+    
+
+    
+}
+
+//Caretaker caretaker = new Caretaker();
+//  Originator originator = new Originator();
+//  originator.setState("State1");
+//  originator.setState("State2");
+//  caretaker.addMemento( originator.save() );
+//  originator.setState("State3");
+//  caretaker.addMemento( originator.save() );
+//  originator.setState("State4");
+//  originator.restore( caretaker.getMemento() );
+
+var array1 = ["a1","b1","c1","d1"]
+var array2 = ["a2","b2","c2","d2"]
+var array3 = ["a3","b3","c3","d3"]
+var array4 = ["a4","b4","c4","d4"]
+var careTaker  = Caretaker()
+var originator = Originator()
+originator.setActionArray(actionArray: array1)
+originator.setActionArray(actionArray: array2)
+careTaker.addMemnto(m: originator.save())
+originator.setActionArray(actionArray: array3)
+careTaker.addMemnto(m: originator.save())
+originator.setActionArray(actionArray: array4)
+originator.restore(m: careTaker.getMemento() )
